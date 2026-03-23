404b85444250d5275a3ff9a21c3a2159c7ac90a7
Patrick de Klein <patrick.deklein@cito.nl>
2026-03-23 22:48:06 +0100

    refactor: update dependencies and clean up imports in Storybook configuration

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,6 @@
 import type { Preview } from '@storybook/web-components-vite';
 
 import '@qti-components/theme/item.css';
-import '@qti-components/base';
 import '../apps/editor/src/qti-editor-app';
 import '../apps/editor/src/style.css';
 
