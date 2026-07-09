6961e129693017bf4b3a9e04df7fdd65d02ee203
Denise Broekman <denise.broekman@cito.nl>
2026-07-09 13:46:23 +0200

    feat: remove rogue dependency on prosekit in hottext

--- packages/prose-qti/src/components/hottext/extensions/index.ts
+++ packages/prose-qti/src/components/hottext/extensions/index.ts
@@ -1,4 +1,1 @@
 export { createHottextWrapSelectionPlugin, HOTTEXT_WRAP_SELECTION_EVENT } from './wrap-selection.js';
-
-// ProseKit extension wrapper (optional - requires prosekit)
-export { defineHottextWrapSelectionExtension } from './prosekit.js';
